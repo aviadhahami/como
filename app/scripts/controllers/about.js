3185633a7c9354e1a3fdaa5fad17e1e6a1aa6025
Aviad Hahami <aviadhahami@gmail.com>
2016-08-18 13:32:33 +0300

    :neckbeard: Commit out of @yeoman boilerplate! GOGOGO

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comoApp
+ */
+angular.module('comoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comoApp
+ */
+angular.module('comoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
