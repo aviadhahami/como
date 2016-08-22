663b54debadc618ceeae97b5ff499ad06a296c4e
Aviad Hahami <aviadhahami@gmail.com>
2016-08-22 12:57:28 +0300

    :construction: Generated pixabay service

---
+++ app/scripts/services/pixabay.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name comoApp.Pixabay
+ * @description
+ * # Pixabay
+ * Service in the comoApp.
+ */
+angular.module('comoApp')
+  .service('Pixabay', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
