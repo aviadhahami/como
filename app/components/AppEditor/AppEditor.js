75a5607c42684213e253526bed8fc26a7ed2ddeb
Aviad Hahami <aviadhahami@gmail.com>
2016-08-22 19:58:44 +0300

    :construction: Working on fetch service

--- app/components/AppEditor/AppEditor.js
+++ app/components/AppEditor/AppEditor.js
@@ -3,9 +3,21 @@
  */
 'use strict';
 
-function AppEditorController(){
+function AppEditorController(Pixabay){
 	console.log(this);
 	let data = this.data;
+	
+	this.imgQuery = null;
+	this.imgID = null;
+	
+	let that = this;
+	this.getByID = function(){
+		data.imgURL = Pixabay.getImageByID(that.imgID);
+	};
+	this.getByQuery = function(){
+		data.imgURL = Pixabay.getImageByQuery(that.imgQuery);
+	};
+	
 }
 
 angular.module('comoApp').component('appEditor', {
--- app/scripts/services/pixabay.js
+++ app/scripts/services/pixabay.js
@@ -7,7 +7,30 @@
  * # Pixabay
  * Service in the comoApp.
  */
+const api = {
+	key: '3146573-d397e7ae9edfec76d75c3fab7',
+	URL: 'https://pixabay.com/api/'
+};
+const getByQueryURI = api.URL+'?key='+api.key+'&query=';
+const getByIdURI = api.URL+'?key='+api.key+'&id=';
+
 angular.module('comoApp')
-  .service('Pixabay', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+	.service('Pixabay', ['$http',function ($http) {
+		return {
+			getImageByQuery: function(query){
+				console.log('Get by Q');
+				let cleanQuery = query.replaceAll(' ','+');
+				$http.get(getByQueryURI+cleanQuery).then( function(data){
+					let previewURL = data.previewURL;
+					return previewURL;
+				});
+			},
+			getImageByID: function(id){
+				console.log('Get by ID');
+				$http.get(getByIdURI+id).then( function(data){
+					let previewURL = data.previewURL;
+					return previewURL;
+				});
+			}
+		};
+	}]);
